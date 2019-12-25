6ef032d379fc49df5071b7d255d78b4940fd3047
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-25 08:20:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,75 @@
 # My Code here....
+def map_to_negativize(arr)
+  result = []
+  i      = 0
+
+  while i < arr.length
+    result << arr[i] * -1
+    i += 1
+  end
+  result
+end
+
+def map_to_no_change(arr)
+  result = []
+  i      = 0
+
+  while i < arr.length
+    result << arr[i]
+    i += 1
+  end
+  result
+end
+
+def map_to_double(arr)
+  result = []
+  i      = 0
+
+  while i < arr.length
+    result << arr[i] * 2
+    i += 1
+  end
+  result
+end
+
+def map_to_square(arr)
+  result = []
+  i      = 0
+
+  while i < arr.length
+    result << arr[i] * arr[i]
+    i += 1
+  end
+  result
+end
+
+def reduce_to_total(arr, starting_point=0)
+  result = starting_point
+  i      = 0
+
+  while i < arr.length
+    result += arr[i]
+    i += 1
+  end
+  result
+end
+
+def reduce_to_all_true(arr)
+  i      = 0
+  result = false
+  while i < arr.length
+    arr[i] ? result = true : result = false
+    i += 1
+  end
+  result
+end
+
+def reduce_to_any_true(arr)
+  i      = 0
+  
+  while i < arr.length
+    result = !!arr[i]
+    i += 1
+  end
+  result
+end
