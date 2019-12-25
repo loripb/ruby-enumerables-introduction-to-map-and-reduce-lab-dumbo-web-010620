d924da7f106de0d88c8faa2c2d354636121c77c6
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-25 08:09:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,7 +58,7 @@ def reduce_to_all_true(arr)
   i      = 0
   result = false
   while i < arr.length
-    arr[i] ? result = true : return result
+    arr[i] ? result = true : return result end
     i += 1
   end
   result
